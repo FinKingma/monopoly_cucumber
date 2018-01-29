6d00b6ef6df4659c38b4cc4b255752b02c0b80fe
Fin Kingma <fkingma@xebia.com>
2018-01-29 12:00:53 +0100

    renamed to monopoly component

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Monopoly from '@/components/Monopoly'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Monopoly',
+      component: Monopoly
     }
   ]
 })
