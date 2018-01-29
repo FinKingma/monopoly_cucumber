3ec71d8f0de5c749efd4e61467c0801acc61c717
Fin Kingma <fkingma@xebia.com>
2018-01-29 12:16:14 +0100

    now also with pretty html report

--- features/support/hooks.js
+++ features/support/hooks.js
@@ -8,24 +8,24 @@ var myHooks = function () {
   })
 
   this.AfterFeatures((scenario, done) => {
-    // var reporter = require('cucumber-html-reporter')
-    // var options = {
-    //   theme: 'bootstrap',
-    //   jsonFile: 'features/reports/report.json',
-    //   output: 'features/reports/index.html',
-    //   reportSuiteAsScenarios: true,
-    //   launchReport: false,
-    //   metadata: {
-    //     'App Version': '0.3.2',
-    //     'Test Environment': 'STAGING',
-    //     'Browser': 'Chrome  54.0.2840.98',
-    //     'Platform': 'Windows 10',
-    //     'Parallel': 'Scenarios',
-    //     'Executed': 'Remote'
-    //   }
-    // }
+    var reporter = require('cucumber-html-reporter')
+    var options = {
+      theme: 'bootstrap',
+      jsonFile: 'features/reports/report.json',
+      output: 'features/reports/index.html',
+      reportSuiteAsScenarios: true,
+      launchReport: false,
+      metadata: {
+        'App Version': '0.3.2',
+        'Test Environment': 'STAGING',
+        'Browser': 'Chrome  54.0.2840.98',
+        'Platform': 'Windows 10',
+        'Parallel': 'Scenarios',
+        'Executed': 'Remote'
+      }
+    }
 
-    // reporter.generate(options)
+    reporter.generate(options)
     done()
   })
 }
